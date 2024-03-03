e6995dc465d84e053e1beafeedde99d5c1954b4f
Anupam2570632 <128147498+Anupam2570632@users.noreply.github.com>
2024-03-03 18:25:45 +0600

    the input field created functional

--- app.js
+++ app.js
@@ -2,13 +2,11 @@ const postCardContainer = document.getElementById('post-card-container');
 const markDiv = document.getElementById('markDiv')
 const loadingSpinner = document.getElementById('loading-spinner');
 const latestPostContainer = document.getElementById('latestPost');
+const inputCategory = document.getElementById('categoryInput');
 
 let bg;
 
-
 const dataLoad = async () => {
-
-
     loadingSpinner.classList.remove('hidden')
 
     setTimeout(() => {
@@ -19,12 +17,11 @@ const dataLoad = async () => {
     const data = await res.json();
     const posts = data.posts;
 
-
-    // postCardContainer.innerHTML = '';
+    postCardContainer.innerHTML = '';
     posts.forEach(post => {
 
         const isActive = post.isActive;
-        console.log(post)
+
         if (isActive) {
             bg = "#10B981";
         }
@@ -81,7 +78,6 @@ const dataLoad = async () => {
 
 dataLoad();
 
-
 let markCount = 0;
 
 const markAsRead = (postTitle, view) => {
@@ -103,8 +99,13 @@ const markAsRead = (postTitle, view) => {
     markDiv.appendChild(div);
 }
 
-
 const loadLatestPost = async () => {
+    loadingSpinner.classList.remove('hidden')
+
+    setTimeout(() => {
+        loadingSpinner.classList.add('hidden')
+    }, 2000);
+
     const res = await fetch('https://openapi.programming-hero.com/api/retro-forum/latest-posts');
     const data = await res.json();
 
@@ -113,9 +114,9 @@ const loadLatestPost = async () => {
 
         let date = post.author.posted_date;
         let designation = post.author.designation;
-        designation = designation? post.author.designation:"Unknown"
+        designation = designation ? post.author.designation : "Unknown"
         date = date ? post.author.posted_date : "No publish date";
-    
+
         div.innerHTML = `
         <div class="flex border border-[#12132D26] flex-col text-start p-6 bg-white rounded-3xl gap-6">
                         <img class="rounded-2xl" src="${post.cover_image}" alt="">
@@ -151,6 +152,80 @@ const loadLatestPost = async () => {
     })
 }
 
-
-
 loadLatestPost();
+
+
+const showDataCategory = async () => {
+    loadingSpinner.classList.remove('hidden')
+
+    setTimeout(() => {
+        loadingSpinner.classList.add('hidden')
+    }, 2000);
+
+    const input = inputCategory.value;
+    const res = await fetch(`https://openapi.programming-hero.com/api/retro-forum/posts?category=${input}`)
+    const data = await res.json();
+
+    postCardContainer.innerHTML = '';
+    const posts = data.posts;
+    if (posts.length === 0) {
+        const div = document.createElement('div');
+        div.innerHTML = `
+        <h1 class="text-center mx-auto text-[40px] text-black font-bold">
+            Data Not Found
+        </h1>
+        `
+        postCardContainer.appendChild(div);
+
+        alert("The category you provide is not a valid category");
+    }
+
+
+    posts.forEach(post => {
+        const div = document.createElement('div');
+
+        div.innerHTML = `
+        <div class="card flex md:flex-row flex-col text-start p-10 gap-6 hover:bg-[#797DFC1A] border duration-300 hover:border-[#797DFC] bg-[#F3F3F5]">
+                                <div class="w-20 relative">
+                                <img class="rounded-2xl w-full" src="${post.image}" alt="">
+                                <div class="w-4 h-4 bg-[${bg}] rounded-full absolute top-[-5px] right-[-5px]">
+
+                                </div>
+                            </div>
+                            <div class="flex-1 space-y-5 ">
+                                <p class="flex gap-4 text-[14px] text-[#12132DCC]">
+                                    <span>#${post.category}</span> <span>Author : ${post.author.name}</span>
+                                </p>
+                                <h1 class="text-[20px] font-bold text-[#12132D]">
+                                    ${post.title}
+                                </h1>
+                                <p class="text-[#12132D99]">
+                                    ${post.description}
+                                </p>
+                                <hr>
+                                <div class="flex justify-between">
+                                    <ul class="flex md:gap-6 gap-3 text-[#12132D99]">
+                                        <li class="flex gap-3 items-center">
+                                            <i class="fa-regular fa-message"></i>
+                                            <span>${post.comment_count}</span>
+                                        </li>
+                                        <li class="flex gap-3 items-center">
+                                            <i class="fa-regular fa-eye"></i>
+                                            <span>${post.view_count}</span>
+                                        </li>
+                                        <li class="flex gap-3 items-center">
+                                            <i class="fa-regular fa-clock"></i>
+                                            <span>${post.posted_time} min</span>
+                                        </li>
+                                    </ul>
+                                    <div onclick="markAsRead('${post.title.replace("'", '')}', '${post.view_count}')"
+                                        class="flex items-center justify-center bg-[#10B981] text-white h-[28px] w-[28px] rounded-full hover:cursor-pointer">
+                                        <i class="fa-solid fa-envelope-open"></i>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+        `
+        postCardContainer.appendChild(div);
+    })
+}
